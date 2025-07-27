545c4938e2b95ecaf6ba520da551c777f7b1838e
hez-zahi <124633794+hez-zah@users.noreply.github.com>
2025-07-27 10:29:52 +0200

    Add files via upload

---
+++ philo/simulation.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sellyani <sellyani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 15:00:00 by sellyani          #+#    #+#             */
+/*   Updated: 2025/07/25 02:48:45 by sellyani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	*moniteur(void *arg)
+{
+	t_data	*data;
+	int	someone_died;
+
+	someone_died = 0;
+	data = (t_data *)arg;
+	if (data->nbr_philo == 1)
+		return (NULL);
+	while (!variable_stop(data))
+	{
+		if (finish_philos(data) || check_dead(data, &someone_died))
+			break ;
+		my_sleep(1);
+	}
+	return (NULL);
+}
+
+void	print_status(t_philo *philo, char *msg)
+{
+	long	now;
+
+	if (break_simulation_aid(philo))
+		return ;
+	pthread_mutex_lock(&philo->data->print);
+	now = get_time() - philo->start_time;
+	printf("%ld %d %s\n", now, philo->id + 1, msg);
+	pthread_mutex_unlock(&philo->data->print);
+}
+
+void    one_philo(t_philo *philo)
+{
+    pthread_mutex_lock(&philo->data->forks[0]);
+    print_status(philo, "has taken a fork");
+    my_sleep(philo->die_time);    // Changed from data->die_time
+    print_status(philo, "died");
+    pthread_mutex_unlock(&philo->data->forks[0]);
+}
+
+void	*routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	if (philo->data->nbr_philo == 1)
+		return (one_philo(philo), NULL);
+	if (!(philo->id % 2))
+		my_sleep(philo->eat_time);
+	while (!variable_stop(philo->data))
+	{
+		if (finish_philos(philo->data) || variable_stop(philo->data))
+			break ;
+		pthread_mutex_lock(&philo->data->forks[philo->id - 1]);
+		if (variable_stop(philo->data))
+		{
+			pthread_mutex_unlock(&philo->data->forks[philo->id - 1]);
+			break ;
+		}
+		if (!routine_aid(philo))
+			break ;
+		do_some(philo, "is thinking");
+		ft_thinking(philo);
+	}
+	return (NULL);
+}
+
+int	start_simulation(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->nbr_philo)
+	{
+		if (pthread_create(&data->philos[i]->thread, NULL, &routine,
+				data->philos[i]))
+			return (0);
+		i++;
+	}
+	if (pthread_create(&data->monitor_thread, NULL,
+			&moniteur, data))
+		return (0);
+	return (1);
+}
---
+++ philo/utlis_2.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sellyani <sellyani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 15:00:00 by sellyani          #+#    #+#             */
+/*   Updated: 2025/07/25 02:48:45 by sellyani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	variable_stop(t_data *data)
+{
+	int	breakk;
+
+	pthread_mutex_lock(&data->stop_mutex);
+	breakk = data->stop;
+	pthread_mutex_unlock(&data->stop_mutex);
+	return (breakk);
+}
+
+size_t	get_time(void)
+{
+	struct timeval	tv;
+
+	if (gettimeofday(&tv, NULL) == -1)
+		return (0);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+}
